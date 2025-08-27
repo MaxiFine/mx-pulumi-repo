649d122cd57255a01850749c69cd924dd189bfa0
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-08-27 10:07:29 +0000

    Initialize webserver project with Pulumi configuration, README, and main script

---
+++ webserver/__main__.py
@@ -0,0 +1,32 @@
+# """An AWS Python Pulumi program"""
+
+# import pulumi
+# from pulumi_aws import s3
+
+# # Create an AWS resource (S3 Bucket)
+# bucket = s3.Bucket('my-bucket')
+
+# # Export the name of the bucket
+# pulumi.export('bucket_name', bucket.id)
+
+import pulumi
+import pulumi_aws as aws
+
+size = 't2.micro'
+ami = aws.ec2.get_ami(most_recent="true",
+                  owners=["137112412989"],
+                  filters=[{"name":"name","values":["amzn2-ami-hvm-*"]}])
+
+group = aws.ec2.SecurityGroup('webserver-secgrp',
+    description='Enable HTTP access',
+    ingress=[
+        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] }
+    ])
+
+server = aws.ec2.Instance('webserver-www',
+    instance_type=size,
+    vpc_security_group_ids=[group.id], # reference security group from above
+    ami=ami.id)
+
+pulumi.export('publicIp', server.public_ip)
+pulumi.export('publicHostName', server.public_dns)
