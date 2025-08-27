9e0d4f157f8b8dc86f5e7dc9f2e8bcdace6c984b
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-08-27 10:33:03 +0000

    Add EC2 instance and security group configuration for webserver

--- webserver/__main__.py
+++ webserver/__main__.py
@@ -11,22 +11,27 @@
 
 import pulumi
 import pulumi_aws as aws
+from pulumi import export
 
 size = 't2.micro'
 ami = aws.ec2.get_ami(most_recent="true",
                   owners=["137112412989"],
                   filters=[{"name":"name","values":["amzn2-ami-hvm-*"]}])
 
-group = aws.ec2.SecurityGroup('webserver-secgrp',
-    description='Enable HTTP access',
+group = aws.ec2.SecurityGroup('sec-group',
+    description='Enable HTTP access and SSH access',
     ingress=[
-        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] }
+        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] },
+        { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0'] }
     ])
 
-server = aws.ec2.Instance('webserver-www',
+server = aws.ec2.Instance('ec2-webserver',
     instance_type=size,
     vpc_security_group_ids=[group.id], # reference security group from above
     ami=ami.id)
 
-pulumi.export('publicIp', server.public_ip)
-pulumi.export('publicHostName', server.public_dns)
+# pulumi.export('publicIp', server.public_ip)
+# pulumi.export('publicIp', server.public_ip)
+export('publicHostName', server.public_dns)
+export('instanceId', server.id)
+export('publicIp', server.public_ip)
